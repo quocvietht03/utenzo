b0497f5c405ce977614f8e71fe84709b27f39e11
tomdevvn90 <tomdevvn90@gmail.com>
2025-10-01 10:40:55 +0700

    create mobile menu widget

--- framework/widgets/frontend.js
+++ framework/widgets/frontend.js
@@ -4,7 +4,30 @@
 	 * @param $ The jQuery alias
 	**/
 
-	/* location list toggle */
+	var SubmenuToggleHandler = function ($scope, $){
+		var hasChildren = $scope.find('.menu-item-has-children');
+
+		hasChildren.each(function () {
+			var $btnToggle = $('<span class="bt-toggle-icon"></span>');
+
+			$(this).append($btnToggle);
+
+			$btnToggle.on('click', function (e) {
+				e.preventDefault();
+
+				if($(this).parent().hasClass('bt-is-active')){
+					$(this).parent().removeClass('bt-is-active');
+					$(this).parent().children('ul').slideUp();
+				} else {
+					$(this).parent().addClass('bt-is-active');
+					$(this).parent().children('ul').slideDown();
+					$(this).parent().siblings().removeClass('bt-is-active').children('ul').slideUp();
+					$(this).parent().siblings().find('li').removeClass('bt-is-active').children('ul').slideUp();
+				}
+			});
+		});
+	}
+
 	var LocationListHandler = function ($scope, $) {
 		var buttonMore = $scope.find('.bt-more-info');
 		var contentList = $scope.find('.bt-location-list--content');
@@ -1016,6 +1039,7 @@
 	}
 	// Make sure you run this code under Elementor.
 	$(window).on('elementor/frontend/init', function () {
+		elementorFrontend.hooks.addAction('frontend/element_ready/bt-mobile-menu.default', SubmenuToggleHandler);
 		elementorFrontend.hooks.addAction('frontend/element_ready/bt-location-list.default', LocationListHandler);
 		elementorFrontend.hooks.addAction('frontend/element_ready/bt-list-faq.default', FaqHandler);
 		elementorFrontend.hooks.addAction('frontend/element_ready/bt-search-product.default', SearchProductHandler);
